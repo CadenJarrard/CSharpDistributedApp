b089f145cf660af02017ef9901367082b4aa8217
Caden02 <jarrardtiger227@yahoo.com>
2023-06-08 22:53:59 -0400

    Initial Project Setup

---
+++ AppController.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpDistApp
+{
+    class AppController
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
