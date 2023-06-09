e745cc0c7b0ef8c64d1b2431567e868bc4caff65
Caden02 <jarrardtiger227@yahoo.com>
2023-06-08 22:59:36 -0400

    Test Commit

--- AppController.cs
+++ AppController.cs
@@ -6,7 +6,7 @@ namespace CSharpDistApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello");
         }
     }
 }
