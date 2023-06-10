9611e28c3b960477edb46dd0eb780d386d185ac6
Caden02 <jarrardtiger227@yahoo.com>
2023-06-10 00:13:13 -0400

    Updated AppController.cs

--- AppController.cs
+++ AppController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 namespace CSharpDistApp
 {
@@ -7,6 +8,29 @@ namespace CSharpDistApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+
+            // Check for correct usage
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage Error: dotnet run <port number> <file name>");
+                Environment.Exit(1);
+            }
+
+            // Retrieve the port number and file name from the command-line arguments
+            int port_number = Int32.Parse(args[0]);
+            string file_name = args[1];
+
+            // Access the dataset file for processing
+            try
+            {
+                FileStream file_stream = File.Open(file_name, FileMode.Open);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File Error: \"{0}\" file could not be found", file_name);
+            }
+            
+
         }
     }
 }
