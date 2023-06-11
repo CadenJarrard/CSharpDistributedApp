0f5533e3262fa60d1d7516520db9ea12f637e347
Caden02 <jarrardtiger227@yahoo.com>
2023-06-10 23:42:42 -0400

    Updated AppController.cs with server socket

--- AppController.cs
+++ AppController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace CSharpDistApp
 {
@@ -28,6 +30,32 @@ namespace CSharpDistApp
             catch (FileNotFoundException)
             {
                 Console.WriteLine("File Error: \"{0}\" file could not be found", file_name);
+            }
+
+            // Start the data slicing thread
+
+            // Create the server socket and listen for incoming connections
+            Socket? server_socket = null;
+            try
+            {
+                // Instantiate server_socket to IPv4 address using TCP protocol
+                server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Create an IPEndPoint and bind to the server_socket
+                IPAddress server_ipAddress = IPAddress.Parse("127.0.0.1");
+                server_socket.Bind(new IPEndPoint(server_ipAddress, port_number));
+
+                // Designate for the server_socket to begin listening for incoming connections
+                server_socket.Listen();
+                Console.WriteLine("Server listening for incoming connections on port# {0}", port_number);
+                
+            }
+            finally // Release resources used by server_socket
+            {
+                if (server_socket != null)
+                {
+                    server_socket.Close();
+                }
             }
             
 
